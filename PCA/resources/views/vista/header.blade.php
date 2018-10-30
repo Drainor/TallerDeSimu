c8319e85b485d6477a377dc65ad7fd571087e643
CarlaOM <30482992+CarlaOM@users.noreply.github.com>
2018-10-30 04:12:49 -0400

    Definicion de etapas del proceso

--- PCA/resources/views/vista/header.blade.php
+++ PCA/resources/views/vista/header.blade.php
@@ -1,31 +1,31 @@
 <table cellspacing="0" cellpadding="0" border="0" width="100%">
   <tr>
-    <td width="20" height="22" valign="top" bgcolor="#336699" background="images/fondotit.png"><img src="images/esquinaiz.gif" width="20" height="22"></td>
-    <td height="22" width="100%" valign="top" bgcolor="#336699" background="images/fondotit.png" class="tituloC hidden-xsn2"><font size="1">
+    <td width="20" height="22" valign="top" bgcolor="#336699" background="images/background1.png"><img src="images/esquinaiz.gif" width="20" height="22"></td>
+    <td height="22" width="100%" valign="top" bgcolor="#336699" background="images/background1.png" class="tituloC hidden-xsn2"><font size="1">
       <h3 style="color:white">
         Universidad Mayor de San Sim&oacute;n
       </h3></font></td>
-    <td width="20" height="22" valign="top" bgcolor="#336699" background="images/fondotit.png" align="right"><img src="images/esquinader.gif" width="20" height="22"></td>
+    <td width="20" height="22" valign="top" bgcolor="#336699" background="images/background1.png" align="right"><img src="images/esquinader.gif" width="20" height="22"></td>
   </tr>
 </table>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr>
     <!-- <object width="99" height="52" data="fcytp.swf">Alertnative Content Here</object> -->
-    <td rowspan="2" background="images/fondotit.png" align="center" valign="middle" width="9" bgcolor="#336699">
+    <td rowspan="2" background="images/background1.png" align="center" valign="middle" width="9" bgcolor="#336699">
       <table class="hidden-xs" width="137" border="0" cellspacing="0" cellpadding="0">
         <tr>
           <td align="center" >
             <!-- <object class="hidden-xs" width="99" height="52" data="images/fcytp.swf">FCyT</object> -->
-            <img src="images/judge_gavel_312468.jpg" class="hidden-xs" width="99" height="52">
+            <img src="images/judge.png" class="hidden-xs" width="100" height="100">
           </td>
         </tr>
       </table>
     </td>
-    <td class="titulo hidden-xsn" rowspan="2" background="images/fondotit.png" align="center" valign="middle" width="312">
+    <td class="titulo hidden-xsn" rowspan="2" background="images/background1.png" align="center" valign="middle" width="312">
         <h1 style="color:white">
           Sistema de Proceso Contencioso Administrativo
         </h1>
     </td>
-    <td background="images/fondotit.png" height="37" valign="top" align="center" class="titulo hidden-xs" width="300"></td>
+    <td background="images/background1.png" height="37" valign="top" align="center" class="titulo hidden-xs" width="300"></td>
   </tr>
 </table>
