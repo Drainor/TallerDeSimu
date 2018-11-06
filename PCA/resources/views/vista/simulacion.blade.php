fc9d2d0df277a5b951731c3a4cf7b8ae43e502be
CarlaOM <30482992+CarlaOM@users.noreply.github.com>
2018-11-06 12:21:59 -0400

    Correcion de ortografia

--- PCA/resources/views/vista/simulacion.blade.php
+++ PCA/resources/views/vista/simulacion.blade.php
@@ -23,7 +23,7 @@
         <th colspan="3">Demanda</th>
       </tr>
       <tr>
-        <td><div class="fat-finger color1 smallipop" id="proceso1">Admision de la Demanda
+        <td><div class="fat-finger color1 smallipop" id="proceso1">Admisión de la Demanda
             <div class="dot-1"></div>
             <!-- <span class="smallipop" id="proceso1"> -->
               <!-- <div class="smallipop-hint">
@@ -54,7 +54,7 @@
         </div>
         </td>
         <td></td>
-        <td><div class="fat-finger color1 smallipop" id="proceso2">Revision de la Demanda
+        <td><div class="fat-finger color1 smallipop" id="proceso2">Revisión de la Demanda
             <div class="dot-2"></div>
             <!-- <div class="smallipop-hint">
               <img src="images/animated-judge-image-0041-tiny.gif" alt="Flying screens"
@@ -84,7 +84,7 @@
       </tr>
       <tr>
         <td></td>
-        <td><div class="fat-finger color1 smallipop" id="proceso3">Utilizacion de Recursos Disponibles
+        <td><div class="fat-finger color1 smallipop" id="proceso3">Utilización de Recursos Disponibles
             <div class="dot-3"></div>
             <!-- <div class="smallipop-hint">
               <img src="images/animated-judge-image-0041-tiny.gif" alt="Flying screens"
@@ -118,10 +118,10 @@
 <div class="fondo">
     <table>
       <tr>
-        <th colspan="3">Tramites de la Via Ordinaria </th>
+        <th colspan="3">Trámites de la Via Ordinaria </th>
       </tr>
       <tr>
-        <td><div class="fat-finger color2 smallipop" id="proceso4">Presentacion de Pruebas
+        <td><div class="fat-finger color2 smallipop" id="proceso4">Presentación de Pruebas
             <div class="dot-4"></div>
                 <!-- <div class="smallipop-hint">
               <img src="images/animated-judge-image-0041-tiny.gif" alt="Flying screens"
@@ -155,7 +155,7 @@
         </div>
         </td>
         <td></td>
-        <td><div class="fat-finger color2 smallipop" id="proceso5">Aprobacion de los Tramites
+        <td><div class="fat-finger color2 smallipop" id="proceso5">Aprobación de los Trámites
             <div class="dot-5"></div>
             <!-- <div class="smallipop-hint">
               <img src="images/animated-judge-image-0041-tiny.gif" alt="Flying screens"
@@ -182,7 +182,7 @@
       </tr>
       <tr>
         <td></td>
-        <td><div class="fat-finger color2 smallipop" id="proceso6">Utilizacion de Recursos Disponibles
+        <td><div class="fat-finger color2 smallipop" id="proceso6">Utilización de Recursos Disponibles
             <div class="dot-6"></div>
             <!-- <div class="smallipop-hint">
               <img src="images/animated-judge-image-0041-tiny.gif" alt="Flying screens"
@@ -218,7 +218,7 @@
         <th colspan="3">Audiencia</th>
       </tr>
       <tr>
-        <td><div class="fat-finger color3 smallipop" id="proceso7">Alegacion por ambas partes
+        <td><div class="fat-finger color3 smallipop" id="proceso7">Alegación por ambas partes
             <div class="dot-7"></div>
             <!-- <div class="smallipop-hint">
               <img src="images/animated-judge-image-0041-tiny.gif" alt="Flying screens"
@@ -247,13 +247,13 @@
         </div>
         </td>
         <td></td>
-        <td><div class="fat-finger color3">Juez dicta Resolucion
+        <td><div class="fat-finger color3">Juez dicta Resolución
             <div class="dot-8"></div>
         </div></td>
       </tr>
       <tr>
         <td></td>
-        <td><div class="fat-finger color3">Utilizacion de Recursos Disponibles
+        <td><div class="fat-finger color3">Utilización de Recursos Disponibles
             <div class="dot-9"></div>
         </div></td>
         <td></td>
@@ -267,12 +267,12 @@
         <th colspan="3">Sentencia </th>
       </tr>
       <tr>
-        <td><div class="fat-finger color3">Emision de la Sentencia
+        <td><div class="fat-finger color3">Emisión de la Sentencia
             <div class="dot-10"></div>
         </div>
         </td>
         <td></td>
-        <td><div class="fat-finger color3">Cumplimiento de la Resolucion
+        <td><div class="fat-finger color3">Cumplimiento de la Resolución
             <div class="dot-11"></div>
         </div></td>
       </tr>
@@ -294,21 +294,21 @@
         </tr>
       <tr>
         <td>
-        <div class="fat-finger color4">Recursos para Tramite
+        <div class="fat-finger color4">Recursos Judiciales de Apelación
             <div class="dot-13"></div>
         </div>
         </td>
         </tr>
       <tr>
         <td>
-        <div class="fat-finger color4">Recurso para Audiencia
+        <div class="fat-finger color4">Recursos Judiciales de Casación
             <div class="dot-14"></div>
         </div>
         </td>
       </tr>
       <tr>
         <td>
-        <div class="fat-finger color4">Recurso para Sentencia
+        <div class="fat-finger color4">Recursos Judiciales de Queja
             <div class="dot-15"></div></div></td>
         </tr>
       <!-- <tr>
